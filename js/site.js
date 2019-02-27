95bbaa0fdac8e4a9e14288598834a2951e5cfb19
msalahy <msalahy@hawk.iit.edu>
2019-02-27 09:43:16 -0600

    New edits
    New Editds

---
+++ js/site.js
@@ -0,0 +1,17 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+    $('html').removeClass('nojs');
+    $('html').addClass('hasjs');
+});
+
+// functions
+function checksubmit(){
+    alert('Submission Received: Congratulations, you are now signed up for our newsletter!');
+    
+    // disable submit button;
+    var submitbutton = document.getElementById("submit");
+    submitbutton.style.opacity = 0.2;
+    submitbutton.style.backgroundColor = lightgrey;
+};
+
+document.getElementById("Test1").addEventListener('submit', checksubmit);
